f1cdb76a4ef86224f313633eb1861c72016be76d
danialcheung <danialcheung1404@gmail.com>
2015-09-21 22:25:45 -0700

    the model layer is ready

---
+++ app/models/User.java
@@ -0,0 +1,24 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import com.avaje.ebean.Model;
+
+@Entity
+public class User extends Model {
+
+    @Id
+    public String email;
+    public String name;
+    public String password;
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public static Finder<String, User> find = new Finder(User.class);
+    public static User authenticate(String email, String password) { 
+        return find.where().eq("email", email) .eq("password", password).findUnique(); }
+}
